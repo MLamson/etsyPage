a8f110875c98f6da6ee5c65178a8578786fc947c
Mark Lamson <lamson_mark@yahoo.com>
2015-01-21 18:17:04 -0500

    working on appending other elements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,22 +2,19 @@ console.log('The Iron Yard Rocks');
 
 
 
-
-var urlLink;
-
-
 etsyItems.results.forEach(function(item){
 
 
 
-	$('.mainSection').append('<a href="' + item.url + '" <img src="' + item.Images[0].url_170x135 + '"></a>');
-
+	$('.mainSection').append('<a href="' + item.url + '" ><img src="' + item.Images[0].url_170x135 + '"></a>');
+	$('.mainSection').append('<span>' + item.title + '</span>');
+	$('.mainSection').append('<span>' + item.login_name + '</span><span>' + item.price + '</span')
 
 
 
 	//$('.mainSection').append('<img src="' + item.Images[0].url_170x135 + '">');
 
-	<a href="samesite.htm"><img src="image.gif"></a>
+	//<a href="samesite.htm"><img src="image.gif"></a>
 
 	
 });
