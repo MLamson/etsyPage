34f8c04255c57732062481b4a5bafafec6e70062
Mark Lamson <lamson_mark@yahoo.com>
2015-01-24 14:35:05 -0500

    item listing done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,9 +16,16 @@ etsyItems.results.forEach(function(item){
 	currencyCode = item.currency_code;
   itemContent = '<div class="itemBox">';
 
-	$('.mainSection').append(itemContent + '<a href="' + item.url + '" ><img src="' + item.Images[0].url_170x135 + '"></a> <span class="titleSpace">' + item.title + '</span><span>' + item.Shop.shop_name + '</span><span>' + item.price + '</span><span>' + currencyCode + '</span>');
-});
+	$('.mainSection').append(itemContent + '<a href="' + item.url + '" ><img src="' + item.Images[0].url_170x135 + '"></a> <a href="' + item.url + '"<span class="titleSpace">' + item.title + '</span></a><a href="' + item.Shop.url + '"><span class="shopName">' + item.Shop.shop_name + '</span></a><span class="curCode">' + currencyCode + '</span><span class="priceColor">' + '$' + item.price + '</span>');
+
 	
+});
+
+//$('imageBox').append('background-image: url(https://github.com/tiy-atl-js-q1-2015/Assignments/blob/master/Assignment%2009%20*/assets/heart.png');
+	
+// $('.itemBox').hover(function(){
+//   $(this).('<img src="heart.png"');
+// 	},function() {$(this)('background-color',"red")});
 
 // 	$('.itemBox').hover(function() {$('this').append'<img src="https://raw.githubusercontent.com/tiy-atl-js-q1-2015/Assignments/master/Assignment%2009/assets/heart.png"'
 // });
